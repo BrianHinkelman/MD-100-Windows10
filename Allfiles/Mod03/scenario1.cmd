e5b9c02db2336e309446dbde6e661dad1ec7b7f4
Brian Hinkelman <brhinkel@microsoft.com>
2019-07-09 10:07:42 -0700

    Rename
    Rename

--- Allfiles/Mod03/scenario1.cmd
+++
@@ -1,5 +0,0 @@
-cls
-echo off
-regedit /s D:\Labfiles\Mod03\Scenario1.reg > nul
-shutdown /r
-cls
---
+++ Allfiles/MonitoringWin10/Mod11.bat
@@ -0,0 +1,6 @@
+md c:\Data
+copy e:\labfiles\Mod11\Data c:\Data
+md c:\Misc
+copy e:\labfiles\Mod11\Misc c:\Misc
+md c:\Reports
+copy e:\labfiles\Mod11\Reports c:\Reports
