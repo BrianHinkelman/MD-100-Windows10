e02e17500d75bbeaafd9ccb385545a446f0c4406
Brian Hinkelman <brhinkel@microsoft.com>
2019-08-20 10:57:32 -0700

    update

---
+++ Labfiles/Support/CopyMonitor.bat
@@ -0,0 +1,3 @@
+md c:\Monitor
+copy \\lon-dc1\e$\labfiles\Support\MonitorScenario.vbs c:\Monitor
+copy \\lon-dc1\e$\labfiles\Support\MonitorScenario.cmd c:\Monitor
--- Labfiles/Support/CopyUserData.bat
+++ Labfiles/Support/CopyUserData.bat
@@ -1,6 +1,6 @@
-md e:\Data
-copy \\lon-dc1\e$\labfiles\Support\Data e:\Data
-md e:\Misc
-copy \\lon-dc1\e$\labfiles\Support\Misc e:\Misc
-md e:\Reports
-copy \\lon-dc1\e$\labfiles\Support\Reports e:\Reports
+md c:\Data
+copy \\lon-dc1\e$\labfiles\Support\Data c:\Data
+md c:\Misc
+copy \\lon-dc1\e$\labfiles\Support\Misc c:\Misc
+md c:\Reports
+copy \\lon-dc1\e$\labfiles\Support\Reports c:\Reports
